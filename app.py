e86c04365ad19c591b399592d1edddc78648f2d8
Michael Robinson <robinson.m.michael@gmail.com>
2021-04-01 17:22:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ last_rows = np.random.randn(1, 1)
 chart = st.line_chart(last_rows)
 
 for i in range(1, 101):
-    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    new_rows = last_rows[-1, :] + np.random.randn(3, 1).cumsum(axis=0)
     status_text.text("%i%% Complete" % i)
     chart.add_rows(new_rows)
     progress_bar.progress(i)
     last_rows = new_rows
-    time.sleep(0.01)
+    time.sleep(0.05)
     
 progress_bar.empty()
 
