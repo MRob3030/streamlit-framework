6e207953e82b2c27b300e69abceae049c8fdbf9b
Michael Robinson <robinson.m.michael@gmail.com>
2021-04-05 14:14:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,16 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-from bokeh.plotting import figure
+from bokeh.plotting import figure, show
 
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
 
-st.title("Stock Price AAPL")
+p = figure(
+    title='simple line example',
+    x_axis_label='x',
+    y_axis_label='y')
+    
+p.line(x, y, legend_label='Trend', line_width=2)
 
-import requests
-import json 
-key = 'EGFI6S3850S7AASC'
-ticker = 'AAPL'
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
-
-response = requests.get(url)
-data = ((response.json()) ) 
-
-
-
-stock = data['Time Series (Daily)']
-df = pd.DataFrame(stock)
-
-date = df.iloc[0,0]
-prices1 = df.iloc[4]
-prices2 = pd.to_numeric(prices1)
-
-plt.subplot(121)
-plt.plot(prices2)
-
-
-plt.ylabel('Price')
-plt.xlabel('Date')
-plt.title('Stock')
-
+st.bokeh_chart(p)
 
 
