e7e157bda313e7912326c8bbaa85f6a558808353
Michael Robinson <robinson.m.michael@gmail.com>
2021-04-05 11:47:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
 
+import numpy as np
 import streamlit as st
+from bokeh.plotting import figure
 
-st.title('Simple Streamlit App')
 
-st.text('Type a number in the box below')
+st.title("Stock Price 5")
 
-n = st.number_input('Number')
+import requests
+import json 
+key = 'EGFI6S3850S7AASC'
+ticker = 'AAPL'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}'.format(ticker, key)
 
-st.write(f'{n} + 1 = {n+1}')
+response = requests.get(url)
+data = ((response.json()) ) 
 
-s = st.text_input('Type a name in the box below')
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
 
-st.write(f'Hello {s}')
+p = figure(
+    title="Stock Price",
+    x_axis_label="time",
+    y_axis_label="price")
 
+p.line(x, y, line_width=3)
+
+#p.xaxis.fixed_location = 0
+#p.yaxis.fixed_location = 0
+
+st.bokeh_chart(p, use_container_width=True)
 
 
 
