f26e25ad876df97783a083cfc977c689d293f242
Michael Robinson <robinson.m.michael@gmail.com>
2021-08-16 21:45:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,24 +11,42 @@ from bokeh.plotting import figure, show
 import requests
 import json 
 import altair as alt
+import time
 
 
-df = pd.read_csv('allow_prob_TC.csv')
+df = pd.read_csv('Documents/allow_prob_TC.csv')
+df_a = pd.read_csv('Documents/Allow_prob_1000.csv')
 
-st.title("Patent Technology")
-#st.text_input("ENTER NO")
+df = df.sort_values(by=['TC'], inplace=False)
+
+df = df.reset_index()
+st.title("Will My Patent Be Granted?")
+
 
             
+st.write("The U.S. Patent and Trademark office publishes datasets including 11 million patent applications. This includes all applications from 1996 to 2018. When each patent application is filed it is assigned to a technology area and classification code. These features were used to create a prediction of the probability of the application being granted. ")
 
-option_tech = st.sidebar.selectbox("Technology", (df['TC']))             
 
+st.write('Select a technology area for your application:')
+
+
+
+        
+option_tech = st.selectbox("Technology", (df['TC']))             
+
+st.spinner()
+with st.spinner(text='Calculating...'):
+    time.sleep(2)
+    st.success('Done')
 
 df['Percent Allowance f'] = (100. * df['Percent Allowance'])
 df['Percent Allowance f'] = (df['Percent Allowance f'].map('{:.0f}%'.format))
 tech_allow = df.loc[df['TC'] == option_tech, 'Percent Allowance f'].values[0]
-tech_allow_per = tech_allow
-'You selected: ', option_tech   
-'The probability that your patent will be approved with 2 years is:', tech_allow
+
+
+'**You selected:** ',  option_tech 
+'**The probability that your patent will be approved with 2 years is:**', tech_allow
+
 
 #st.bar_chart(df[['Percent Allowance', 'TC']]) 
 
@@ -57,3 +75,15 @@ c = alt.layer(bars, line, data=df)
 
 
 st.altair_chart(c, use_container_width=True)
+
+
+option_c = st.selectbox("Classification", (df_a['uspc_class']))   
+
+
+df_a['Percent Allowance f'] = (100. * df_a['allowance'])
+df_a['Percent Allowance f'] = (df_a['Percent Allowance f'].map('{:.0f}%'.format))
+cpc_allow = df_a.loc[df_a['uspc_class'] == option_c, 'Percent Allowance f'].values[0]
+
+
+'**You selected:** ', option_c   
+'**The probability that your patent will be approved with 2 years is:**', cpc_allow
